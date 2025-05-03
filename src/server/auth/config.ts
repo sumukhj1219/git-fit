328d38579acc004d7e7327a909b32492767fbd15
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-05-03 10:18:28 +0530

    added : auth secret

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -33,8 +33,8 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     GithubProvider({
-      clientId:process.env.AUTH_GITHUB_ID,
-      clientSecret:process.env.AUTH_GITHUB_SECRET
+      clientId:process.env.GITHUB_ID,
+      clientSecret:process.env.GITHUB_SECRET
     }),
     /**
      * ...add more providers here.
