82adb7ddf18d46fc38eca1531bd4e8eafd8eb9f4
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-05-03 09:39:44 +0530

    added: auth

--- src/app/(chat)/chat/page.tsx
+++ src/app/(chat)/chat/page.tsx
@@ -1,8 +1,14 @@
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 import React from 'react'
 import ChatInput from '~/app/_components/chats/chat-input'
 
 
 const page = () => {
+  const {data:session} = useSession()
+  if(!session?.user.id){
+    redirect("/")
+  }
   return (
     <div>
       <ChatInput />
